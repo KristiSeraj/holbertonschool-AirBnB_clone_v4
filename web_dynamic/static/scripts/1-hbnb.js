7319e315d8eac94928c2225ac17c1da575654773
KristiSeraj <serajkristi@gmail.com>
2022-09-23 04:33:43 -0700

    amenity list

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-
-}
+$(document).ready(function () {
+  const amenityDictionary = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).is(':checked')) {
+      amenityDictionary[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDictionary[$(this).attr('data-id')];
+    }
+    console.log(amenityDictionary);
+  });
+});
