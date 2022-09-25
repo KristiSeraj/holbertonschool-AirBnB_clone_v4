04dc5f22d429327254f9ba0064c1b4c08d76f00d
KristiSeraj <serajkristi@gmail.com>
2022-09-25 10:45:33 -0700

    test

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -20,29 +20,29 @@ $(document).ready(function () {
   });
 
   $.ajax({
-	  url: apiUrl + ':5001/api/v1/places_search/',
-	  type: "POST",
-	  data: '{}',
-	  dataType: "json",
-	  contentType: "application/json",
-	  success: function(data) {
-		  data.map(place => {
-		  	$('section.places').append(`
-			<article>
-		  		<div class="title_box">
-		  			<h2>${ place.name }</h2>
-		  			<div class="price_by_night">$ ${ place.price_by_night }</div>
-		  		</div>
-	  			<div class="information">
-	    				<div class="max_guest">${ place.max_guest } Guest ${ place.max_guest != 1 ? 's' : ''}</div>
-            				<div class="number_rooms">${ place.number_rooms } Bedroom ${ place.number_rooms != 1 ? 's' : ''}</div>
-            				<div class="number_bathrooms">${ place.number_bathrooms } Bathroom ${ place.number_bathrooms != 1 ? 's' : ''}</div>
-	  			</div>
-          			<div class="description">
-					${ place.description }
-          			</div>
-			</article>`)
-		  })
-	  }
-  })
+    url: apiUrl + ':5001/api/v1/places_search/',
+    type: 'POST',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      data.map(place => {
+        $('section.places').append(`
+		<article>
+	<div class="title_box">
+			<h2>${place.name}</h2>
+			<div class="price_by_night">$ ${place.price_by_night}</div>
+		</div>
+	  	<div class="information">
+	    		<div class="max_guest">${place.max_guest} Guest ${place.max_guest !== 1 ? 's' : ''}</div>
+            		<div class="number_rooms">${place.number_rooms} Bedroom ${place.number_rooms !== 1 ? 's' : ''}</div>
+            		<div class="number_bathrooms">${place.number_bathrooms} Bathroom ${place.number_bathrooms !== 1 ? 's' : ''}</div>
+		</div>
+          	<div class="description">
+			${place.description}
+          	</div>
+	</article>`);
+      });
+    }
+  });
 });
